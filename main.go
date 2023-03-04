b02d3d80f14bf1261ab26fc212ea4bf86b7ed460
Shaik Zhafir <shaikzhafir@Shaiks-MacBook-Air.local>
2023-03-04 14:51:50 +0800

    feat: added context by just dumping all prev chat history lol

--- main.go
+++ main.go
@@ -1,30 +1,30 @@
 package main
 
 import (
+	"bufio"
+	"chat-backend/clientService"
 	"context"
-	gogpt "github.com/sashabaranov/go-gpt3"
 	"os"
 )
 
 func main() {
-	authKey := os.Getenv("AUTH_KEY")
-	c := gogpt.NewClient(authKey)
 	ctx := context.Background()
-	req := gogpt.ChatCompletionRequest{
-		Model: gogpt.GPT3Dot5Turbo,
-		Messages: []gogpt.ChatCompletionMessage{
-			{
-				Role:    "user",
-				Content: "never say the following sentence again: 'I apologize, but as an AI language model, I don't have access to the previous conversation or topic.'",
-			},
-		},
-		MaxTokens: 1000,
-		User:      "user",
+	c := clientService.NewClientService()
+	var promptChan = make(chan string)
+
+	scanner := bufio.NewScanner(os.Stdin)
+	go func() {
+		for scanner.Scan() {
+			promptChan <- scanner.Text()
+		}
+	}()
+
+	for {
+		select {
+		case prompt := <-promptChan:
+			// append prompt to chat completion request
+			// and send it to the stream
+			c.SendPromptToStream(ctx, prompt)
+		}
 	}
-	resp, err := c.CreateChatCompletion(ctx, req)
-	if err != nil {
-		panic(err)
-	}
-	println(req.Messages[0].Content)
-	println(resp.Choices[0].Message.Content)
 }
